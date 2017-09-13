8e440b14344c4ddbe47aadec5491d168d89da38d
Dov Bluming <dovybluming@gmail.com>
2017-09-13 16:39:10 -0400

    changed controller to use repositry dada

--- ComicBookGallery2/Controllers/ComicBookController.cs
+++ ComicBookGallery2/Controllers/ComicBookController.cs
@@ -3,30 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ComicBookGallery2.Data;
 using ComicBookGallery2.Models;
 
 namespace ComicBookGallery2.Controllers
 {
     public class ComicBookController : Controller
     {
-        public ActionResult Details()
-        {
-            ComicBook comicBook = new ComicBook()
-            {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-                    new Artist() { Name = "Dan Slott", Role = "Script"},
-                    new Artist() { Name = "Humberto Ramos", Role = "Pencils"},
-                    new Artist() { Name = "Victor Olazaba", Role = "Inks"},
-                    new Artist() { Name = "Edgar Delgado", Role = "Colors"},
-                    new Artist() { Name = "Chris Eliopoulos", Role = "Letters"}
-                },
-                Favorite = true
-            };
+        private ComicBooksRepository _comicBookRepository = null;
 
+        public ComicBookController()
+        {
+            _comicBookRepository = new ComicBooksRepository();
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            var comicBook = _comicBookRepository.GetComicBook((int)id);
             return View(comicBook);
         }
     }

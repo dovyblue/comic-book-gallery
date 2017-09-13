dbde1982a8316287eb100bb4df2f9701d27b7502
Dov Bluming <dovybluming@gmail.com>
2017-09-12 22:23:05 -0400

    added ComicBook and Artist models

---
+++ ComicBookGallery2/Models/ComicBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery2.Models
+{
+    public class ComicBook
+    {
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Artist[] Artists { get; set; }
+    }
+}
